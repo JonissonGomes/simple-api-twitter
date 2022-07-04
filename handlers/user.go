629bf6566dbb2087d3cef22b2424003aae667fb9
jonisson.gomes <jonisson.gomes@g.globo>
2022-07-04 18:00:02 -0300

    Creating user connect

--- handlers/user.go
+++ handlers/user.go
@@ -1,10 +1,18 @@
 package handlers
 
 import (
+	"github.com/JonissonGomes/simple-api-twitter/model"
 	"github.com/labstack/echo/v4"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func (h *Handler) SignUp(c echo.Context) (err error) {
+
+	// User connect
+	user := &model.User{ID: bson.NewObjectId()}
+	if err = c.Bind(user); err != nil {
+		return
+	}
 
 	return
 }
