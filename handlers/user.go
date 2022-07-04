fa78a997bd465c6ec8dc585cc9d8a28011325972
jonisson.gomes <jonisson.gomes@g.globo>
2022-07-04 18:04:35 -0300

    User validation

--- handlers/user.go
+++ handlers/user.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/JonissonGomes/simple-api-twitter/model"
 	"github.com/labstack/echo/v4"
 	"gopkg.in/mgo.v2/bson"
@@ -12,6 +14,11 @@ func (h *Handler) SignUp(c echo.Context) (err error) {
 	user := &model.User{ID: bson.NewObjectId()}
 	if err = c.Bind(user); err != nil {
 		return
+	}
+
+	// User validation
+	if user.Email == "" || user.Password == "" {
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: "Email ou senha não conferem"}
 	}
 
 	return
