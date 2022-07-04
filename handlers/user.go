d97c13f5963d7bfbff65f2a1b8a178ad073da312
jonisson.gomes <jonisson.gomes@g.globo>
2022-07-04 18:19:38 -0300

    Searching user on database

--- handlers/user.go
+++ handlers/user.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/JonissonGomes/simple-api-twitter/model"
 	"github.com/labstack/echo/v4"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -35,6 +36,16 @@ func (h *Handler) Login(c echo.Context) (err error) {
 	// User connect
 	user := &model.User{ID: bson.NewObjectId()}
 	if err = c.Bind(user); err != nil {
+		return
+	}
+
+	// Search user
+	db := h.DB.Clone()
+	defer db.Close()
+	if err = db.DB("twitter-api").C("users").Find(bson.M{"email": user.Email, "password": user.Password}).One(user); err != nil {
+		if err == mgo.ErrNotFound {
+			return &echo.HTTPError{Code: http.StatusUnauthorized, Message: "Email ou senha não conferem"}
+		}
 		return
 	}
 
